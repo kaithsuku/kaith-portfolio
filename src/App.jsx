04d3abc8fc1737fb0771c84612d2cfcf364fcae2
Kaith Suraj <90097040+kaithsuku@users.noreply.github.com>
2025-01-24 18:12:25 +0530

    Added Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './sections/Projects'
 import Clients from './sections/Clients'
 import Contact from './sections/Contact'
 import Footer from './sections/Footer'
+import Experience from './sections/Experience'
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Hero />      
       <About />
       <Projects />
-      <Clients />
+      {/* <Clients /> */}
+      <Experience />
       <Contact />
       <Footer />
     </main>
